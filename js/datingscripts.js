f8bccbf47cd05e804ba6b2c7561b0a641525c48b
Kayl Eubanks and Willam Chu <willam@>
2018-03-22 15:34:38 -0700

    Add CSS styles, lucky logic, Gottfried logic

--- js/datingscripts.js
+++ js/datingscripts.js
@@ -4,20 +4,48 @@ $(function(){
     var food = parseInt($("input:radio[name=food]:checked").val());
     var color = parseInt($("input:radio[name=color]:checked").val());
     var datingScore = movie + food + color;
-    console.log(datingScore);
+    console.log(preference);
+    var preference = $("#preference").val();
+    console.log(preference);
 
-    $(".results").show();
-    if (datingScore <= 3) {
-      console.log("steve should show now");
-      $(".steve").show();
-    } else if (datingScore >3 && datingScore <7){
-      console.log("nicki should show now");
-      $(".nicki").show();
-    } else {
-      console.log("tilda should show now");
-      $(".tilda").show();
+// Randomize gender result for those feeling lucky
+    if (preference === "lucky") {
+      var random = Math.random();
+      if (random <= 0.5) {
+        preference = "male";
+        console.log(preference);
+      } else {
+        preference = "female";
+        console.log(preference);
+      }
     }
+// Shows results well
+    $(".results").show();
+// Gottfried logic
+    if (preference === 'loser') {
+     $(".loser").show();
+// Shows match for male
+    } else if (preference === "male") {
+      if (datingScore <= 4) {
+        $(".steve").show();
+      } else if (datingScore >4 && datingScore <7){
+        $(".therock").show();
+      } else {
+        $(".prince").show();
+      }
+// Shows match for female
+    } else {
+      if (datingScore <= 4) {
+        $(".schaal").show();
+      } else if (datingScore >4 && datingScore <7){
+        $(".nicki").show();
+      } else {
+        $(".tilda").show();
+      }
+    }
+// Shows match for female
 
+// Hide survey
     $("#datingSurvey").hide();
 
     event.preventDefault();
